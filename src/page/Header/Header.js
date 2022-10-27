945de667c1c00eb3f3659d09fde5fb739df2d881
Md Hasanul Karim <hasanlink71@yahoo.com>
2022-10-27 14:07:23 +0600

    header changed

--- src/page/Header/Header.js
+++ src/page/Header/Header.js
@@ -39,20 +39,9 @@ const Header = () => {
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="me-auto">
-                <Nav.Link><Link to='/'>Home</Link></Nav.Link>
-                <Nav.Link><Link to='/coursepage'>Courses</Link></Nav.Link>
-                <Nav.Link href="#home">FAQ</Nav.Link>
-                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">
-                    Another action
-                  </NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item href="#action/3.4">
-                    Separated link
-                  </NavDropdown.Item>
-                </NavDropdown>
+                <Nav.Link><Link to='/'><button className='btn btn-light'>Home</button></Link></Nav.Link>
+                <Nav.Link><Link to='/coursepage'><button className='btn btn-light'>Courses</button></Link></Nav.Link>
+                <Nav.Link><Link to='/faq'><button className='btn btn-light'>FAQ</button></Link></Nav.Link>
               </Nav>
               <div className='d-flex'>
                 {
