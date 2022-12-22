909a3a04c12a2e5577b473ab32972ba80f75a697
Md Hasanul Karim <hasanlink71@yahoo.com>
2022-12-22 21:03:32 +0600

    banner and course info changed

--- src/page/Home/Home.js
+++ src/page/Home/Home.js
@@ -7,6 +7,10 @@ const Home = () => {
       <h1 className='display-1 fw-bolder'>Welcome to Alpha Academy!</h1>
       <h2>Learn without limits</h2>
       <p className='fs-3'>Start, switch, or advance your career with our courses, Professional Certificates, and degrees from world-class universities and companies.</p>
+      <div>
+        <button type="button" class="btn btn-primary btn-lg">Go Course</button>
+        <button type="button" class="btn btn-lg btn-outline-primary ms-2">Learn More</button>
+      </div>
     </div>
   );
 };
